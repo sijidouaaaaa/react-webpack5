0ea4b871fdbcb243a29fb353b52d2c5931f7f8c6
sijido <2058016038@qq.com>
2024-04-09 20:53:36 +0800

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 // 入口文件 
-import React from 'react';
+
 import { createRoot } from 'react-dom/client'; // 18 用法
 //加载页面
 import AppRouter from './router.jsx'; // 载入路由
 
-import App from './app.js';
+
 
 import {setTimeDiff} from './common/tool.js';
 
@@ -14,5 +14,4 @@ setTimeDiff(document.getElementById('app').getAttribute('data-date'));
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-// root.render(<App/>);
 root.render(AppRouter);
